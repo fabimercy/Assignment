8445bc91375292e1d6b0838baec985d985eefcbc
Ishwarigt <155798528+Ishwarigt@users.noreply.github.com>
2024-03-01 13:57:06 +0530

    committed successfully

--- SmartCalculator.java
+++ SmartCalculator.java
@@ -1,61 +1,119 @@
 package simple_interest;
 import java.util.Scanner;
 
+
+/**
+ *  A SmartCalculator class that provides various mathematical calculations.
+ */
+
 public class SmartCalculator {
     private double principal;
     private double rate;
     private double time;
 
-    // Constructors
+/**
+ * Constructs a SmartCalculator object with default values for principal, rate, and time.
+ */
     public SmartCalculator() {
         this.principal = 0;
         this.rate = 0;
         this.time = 0;
     }
-
+    /**
+     * Constructs a SmartCalculator object with specified values for principal, rate, and time.
+     *
+     * @param principal The principal amount.
+     * @param rate      The rate of interest.
+     * @param time      The time period.
+     */
     public SmartCalculator(double principal, double rate, double time) {
         this.principal = principal;
         this.rate = rate;
         this.time = time;
     }
+    /**
+     * Gets the principal amount.
+     *
+     * @return The principal amount.
+     */
 
     
     public double getPrincipal() {
         return principal;
     }
-
+    /**
+     * Sets the principal amount.
+     *
+     * @param principal The principal amount to set.
+     */
     public void setPrincipal(double principal) {
         this.principal = principal;
     }
-
+    /**
+     * Gets the rate of interest.
+     *
+     * @return The rate of interest.
+     */
     public double getRate() {
         return rate;
     }
-
+    /**
+     * Sets the rate of interest.
+     *
+     * @param rate The rate of interest to set.
+     */
     public void setRate(double rate) {
         this.rate = rate;
     }
 
+    /**
+     * Gets the time period.
+     *
+     * @return The time period.
+     */
     public double getTime() {
         return time;
     }
 
+    /**
+     * Sets the time period.
+     *
+     * @param time The time period to set.
+     */
     public void setTime(double time) {
         this.time = time;
     }
 
+    /**
+     * Calculates the simple interest.
+     *
+     * @param principal The principal amount.
+     * @param rate      The rate of interest.
+     * @param time      The time period.
+     * @return The simple interest.
+     */
     
     public double calculateSimpleInterest(double principal, double rate, double time) {
         return (principal * rate * time) / 100;
     }
 
-    
+
+    /**
+     * Calculates the compound interest.
+     *
+     * @return The compound interest.
+     */ 
     public double calculateCompoundInterest() {
         double amount = principal * Math.pow((1 + rate / 100), time);
         return amount - principal;
     }
 
-    
+    /**
+     * Calculates the mean of an array of numbers.
+     *
+     * @param numbers The array of numbers.
+     * @return The mean of the numbers.
+     */
     public double calculateMean(double[] numbers) {
         double sum = 0;
         for (double num : numbers) {
@@ -64,7 +122,12 @@ public class SmartCalculator {
         return sum / numbers.length;
     }
 
-    
+    /**
+     * Calculates the factorial of a number.
+     *
+     * @param n The number for which factorial is to be calculated.
+     * @return The factorial of the number.
+     */
     public int calculateFactorial(int n) {
         if (n == 0)
             return 1;
@@ -72,10 +135,21 @@ public class SmartCalculator {
             return n * calculateFactorial(n - 1);
     }
 
+    /**
+     * Calculates the percentage.
+     *
+     * @param obtainedMarks The marks obtained.
+     * @param totalMarks    The total marks.
+     * @return The percentage.
+     */
     public double calculatePercentage(double obtainedMarks, double totalMarks) {
         return (obtainedMarks / totalMarks) * 100;
     }
-
+    /**
+     * Main method to test the SmartCalculator class.
+     *
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         SmartCalculator calculator = new SmartCalculator();
